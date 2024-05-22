76ba293a327cf09cb12f79681322ff16e334aadf
Stephan Sure <stephan.sure@gmail.com>
2024-05-22 18:17:34 +0000

    Add success message after registration

--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views import generic
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
+from django.contrib import messages
 
 # Create your views here.
 
@@ -9,3 +10,10 @@ class UserCreationView(generic.CreateView):
     form_class = UserCreationForm
     template_name = 'registration/register.html'
     success_url = reverse_lazy('login')
+    success_message = "User Created Successfully"
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        messages.success(self.request, self.success_message)
+        return response
+            
