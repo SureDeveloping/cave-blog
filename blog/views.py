161d1952122c2be0107e763a9117d0e052f30cd9
Stephan Sure <stephan.sure@gmail.com>
2024-05-23 09:26:04 +0000

    Change ordering of the blog posts

--- blog/views.py
+++ blog/views.py
@@ -10,7 +10,7 @@ from django.urls import reverse_lazy
 class HomeView(ListView):
     model = Post
     template_name = 'blog/index.html'
-    ordering = ['created_on']
+    ordering = ['-created_on']
 
 # Datail view for one blog posts
 class BlogPostDetailView(DetailView):
