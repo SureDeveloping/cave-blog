c0c14aea4ed7cf2c97bc3122fd7ff578a6ef34a2
Stephan Sure <stephan.sure@gmail.com>
2024-05-24 22:18:00 +0000

    Add user profile detail view

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import UserCreationView, UserProfileCreationView
+from .views import UserCreationView, UserProfileCreationView, ProfilePageDetailView
 
 urlpatterns = [
     path('register/', UserCreationView.as_view(), name='register'),
     path('create-user-profil', UserProfileCreationView.as_view(), name='user-profil-create'),
+    path('user-profil/<int:pk>/', ProfilePageDetailView.as_view(), name='user-profil-detail'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404 
 from django.views import generic
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.contrib import messages
 from . forms import RegisterForm, UserProfileForm
 from . models import Profile
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DetailView
 
 # Create your views here.
 
@@ -28,3 +28,14 @@ class UserProfileCreationView(CreateView):
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super().form_valid(form)
+
+class ProfilePageDetailView(DetailView):
+    model = Profile
+    template_name = 'registration/user_profile_detail.html'
+
+    def get_context_data(self, *args, **kwargs):
+        users = Profile.objects.all()
+        context = super().get_context_data(*args, **kwargs)
+        profile_page_user = get_object_or_404(Profile, id=self.kwargs['pk'])
+        context["profile_page_user"] = profile_page_user
+        return context
