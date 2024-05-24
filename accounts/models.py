fbdf54ea4ac9d516befe7f35ec48606146aeb02d
Stephan Sure <stephan.sure@gmail.com>
2024-05-24 16:48:04 +0000

    Add user model extension, profile model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, null=True, on_delete = models.CASCADE)
+    city = models.CharField(max_length=100, blank=True, null=True)
+    country = models.CharField(max_length=100, blank=True, null=True)
+    userbio = models.TextField()
+    website_url = models.CharField(max_length=200, blank=True, null=True)
+    facebook_url = models.CharField(max_length=200, blank=True, null=True)
+    instagram_url = models.CharField(max_length=200, blank=True, null=True)
+    profile_picture = models.ImageField(upload_to='images/profile', blank=True, null=True)
+
+    def __str__(self):
+        return self.user.username + ' Profile'
+
+    def get_absolute_url(self):
+        return reverse('home')
